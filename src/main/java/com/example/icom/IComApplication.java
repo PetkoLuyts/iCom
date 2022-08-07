cc826638032c50d833fca072410c2cbac9ca15f8
petko.lyutskanov <simona2000petko@abv.bg>
2022-08-07 23:45:33 +0300

    Initial commit

---
+++ src/main/java/com/example/icom/IComApplication.java
@@ -0,0 +1,13 @@
+package com.example.icom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IComApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IComApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/icom/IComApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.icom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IComApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
